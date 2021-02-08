8f7481dce2f1334f57138ac913c3b889f0be81cf
Aaron Huggins <ahuggins@nethealthshops.com>
2021-02-08 09:49:11 -0600

    Reorganize code and handle markdown

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import { Component, ConverterComponent } from 'typedoc/dist/lib/converter/compon
 import { Converter } from 'typedoc/dist/lib/converter/converter'
 import { Context } from 'typedoc/dist/lib/converter/context'
 import { ParameterType, PluginHost } from 'typedoc/dist/lib/utils'
+import { MarkedPlugin } from 'typedoc/dist/lib/output/plugins/MarkedPlugin'
 import * as YAML from 'js-yaml'
-import { CommentTag } from 'typedoc/dist/lib/models/comments/tag'
 import { format } from './jsonTable'
 
 export interface OpenApiDocOpts {
-  /** Hoists the summary and description found in the swagger/openapi path or operation to the signature description. Defaults to true. */
+  /** Hoists the summary and description found in the swagger/openapi path or operation to the signature description; defaults to true. */
   hoistDescription?: boolean
-  /** Render the swagger/openapi tag as a code block instead of additional tags. Defaults to false. */
-  renderYaml?: boolean
+  /** Render the swagger/openapi tag as pretty HTML instead of a code block; defaults to true. */
+  yaml2Html?: boolean
   /** Rename the tag from 'swagger' or 'openapi' to something else. Defaults to true, enforcing 'openapi'; provide a different name or set to false to disable. */
   renameTag?: boolean | string
 }
@@ -27,10 +27,12 @@ export interface OpenApiPath {
   [operation: string]: OpenApiOperation | string
 }
 
+type OpenApiOps = Array<{ method: string, metadata: OpenApiOperation}>
+
 export const PLUGIN_NAME = 'openapi-doc'
 const DEFAULT_OPTIONS: OpenApiDocOpts = {
   hoistDescription: true,
-  renderYaml: false,
+  yaml2Html: true,
   renameTag: true
 }
 const SUPPORTED_METHODS = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace']
@@ -50,12 +52,18 @@ export function load (host: PluginHost): void {
 
 @Component({ name: PLUGIN_NAME })
 export class OpenApiDocPlugin extends ConverterComponent {
+  private generatedComment: string
   private options: OpenApiDocOpts
+  private marked: MarkedPlugin
 
   initialize (): void {
     this.listenTo(this.owner, {
       [Converter.EVENT_CREATE_SIGNATURE]: this.onSignature
     })
+
+    this.marked = new MarkedPlugin(this.application.renderer)
+
+    this.generatedComment = '<!-- Generated from @openapi --><br>'
 
     try {
       const userOptions: any = this.owner.application.options.getValue(PLUGIN_NAME)
@@ -75,7 +83,6 @@ export class OpenApiDocPlugin extends ConverterComponent {
   }
 
   private onSignature (context: Context, reflection: Reflection, node?: any): void {
-    const generatedComment = '<!-- Generated from @openapi --><br>'
     console.log('declaration')
 
     if (typeof reflection.comment !== 'undefined') {
@@ -84,12 +91,12 @@ export class OpenApiDocPlugin extends ConverterComponent {
           ? reflection.comment.getTag('swagger')
           : reflection.comment.getTag('openapi')
 
-        if (!openApi.text.startsWith(generatedComment)) {
+        if (!openApi.text.startsWith(this.generatedComment)) {
           // Gather data from swagger YAML comment.
           const openApiObj: any = YAML.load(openApi.text)
+          const operations: OpenApiOps = []
           let openApiPath: OpenApiPath = {}
           let openApiPathName: string = ''
-          const operations: Array<{ method: string, metadata: OpenApiOperation}> = []
 
           for (const [key, value] of Object.entries(openApiObj)) {
             if (typeof value === 'object') {
@@ -139,52 +146,68 @@ export class OpenApiDocPlugin extends ConverterComponent {
             openApi.tagName = this.options.renameTag
           }
 
-          if (this.options.renderYaml) {
-            openApi.text = `${generatedComment}<pre>${openApi.text}</pre>`
+          let rendered = ''
+
+          if (this.options.yaml2Html) {
+            rendered = this.renderYaml2Html(openApiPathName, operations)
           } else {
-            // reflection.comment.removeTags(openApi.tagName)
-            const htmlDescription: string[] = [generatedComment]
-            // const addHtmlPart = (name: string, html: string = '') => htmlDescription.push(`<div><h4>${name}<h4>${html}</div>`)
-            htmlDescription.push('<div>')
-            // htmlDescription.push(`<div class="lead"><p>Endpoint: <code>${openApiPathName}</code></p></div>`)
-
-            for (const item of operations) {
-              htmlDescription.push('<style>')
-              htmlDescription.push(`label.for-${item.method}{cursor:pointer;}`)
-              htmlDescription.push(`input[type=checkbox]#toggle-${item.method}{position:absolute;top:0;left:0;width:1px;height:1px;opacity:0;}`)
-              htmlDescription.push(`.control-${item.method}{max-height:0;overflow:hidden;transition:max-height 0.4s ease;}`)
-              htmlDescription.push(`input[type=checkbox]#toggle-${item.method}:checked ~ .control-${item.method}{max-height:unset;}`)
-              htmlDescription.push('</style>')
-              htmlDescription.push(`<label class="for-${item.method}" for="toggle-${item.method}">`)
-              htmlDescription.push(`<h3>${item.method.toUpperCase()} <code>${openApiPathName}</code></h3>`)
-              htmlDescription.push('</label>')
-              htmlDescription.push(`<input type="checkbox" id="toggle-${item.method}">`)
-              htmlDescription.push(`<div class="control-${item.method}">`)
-              for (const [key, value] of Object.entries(item.metadata)) {
-                htmlDescription.push('<div>')
-                htmlDescription.push(`<h4 style="font-weight:bold;">${key[0].toUpperCase() + key.slice(1)}</h4>`)
-                htmlDescription.push('<div>')
-                htmlDescription.push(typeof value === 'object' ? format(value) : `<p>${value}</p>`)
-                htmlDescription.push('</div>')
-                htmlDescription.push('</div>')
-                /* const tag: CommentTag = new CommentTag(
-                  openApi.tagName + ' ' + item.method.toUpperCase() + ' ' + key,
-                  '',
-                  typeof value === 'object' ? format(value) : value
-                )
-
-                reflection.comment.tags.push(tag) */
-              }
-              htmlDescription.push('</div>')
-            }
-
-            htmlDescription.push('</div>')
-
-            openApi.text = htmlDescription.join('')
-            console.log(openApi.text)
+            rendered = this.renderMarkdown(`\`\`\`yaml${openApi.text}\n\`\`\``)
           }
+
+          openApi.text = `${this.generatedComment}${rendered}`
         }
       }
     }
+  }
+
+  private renderMarkdown (markdown: string): string {
+    const markedEvent = { parsedText: markdown }
+
+    this.marked.onParseMarkdown(markedEvent as any)
+
+    return markedEvent.parsedText
+  }
+
+  private renderYaml2Html (pathName: string, operations: OpenApiOps): string {
+    const htmlDescription: string[] = []
+
+    htmlDescription.push('<div>')
+
+    for (const item of operations) {
+      htmlDescription.push('<style>')
+      htmlDescription.push(`label.for-${item.method}{cursor:pointer;}`)
+      htmlDescription.push(`label.for-${item.method}:before{content:'\\FF0B';font-size:24px;font-weight:bold;margin-right:2px;color:black;float:left;}`)
+      htmlDescription.push(`input[type=checkbox]#toggle-${item.method}:checked ~ label.for-${item.method}:before{content:'\\FF0D';}`)
+      htmlDescription.push(`input[type=checkbox]#toggle-${item.method}{position:absolute;top:0;left:0;width:1px;height:1px;opacity:0;}`)
+      htmlDescription.push(`.control-${item.method}{max-height:0;overflow:hidden;transition:max-height 0.4s ease;}`)
+      htmlDescription.push(`input[type=checkbox]#toggle-${item.method}:checked ~ .control-${item.method}{max-height:unset;}`)
+      htmlDescription.push('</style>')
+      htmlDescription.push(`<input type="checkbox" id="toggle-${item.method}">`)
+      htmlDescription.push(`<label class="for-${item.method}" for="toggle-${item.method}">`)
+      htmlDescription.push(`<h3>${item.method.toUpperCase()} <code>${pathName}</code></h3>`)
+      htmlDescription.push('</label>')
+      htmlDescription.push(`<div class="control-${item.method}">`)
+
+      for (const [key, value] of Object.entries(item.metadata)) {
+        htmlDescription.push('<div>')
+        htmlDescription.push(`<h4 style="font-weight:bold;">${key[0].toUpperCase() + key.slice(1)}</h4>`)
+        htmlDescription.push('<div>')
+
+        if (typeof value === 'object') {
+          htmlDescription.push(format(value))
+        } else {
+          htmlDescription.push(this.renderMarkdown(`${value}`))
+        }
+
+        htmlDescription.push('</div>')
+        htmlDescription.push('</div>')
+      }
+
+      htmlDescription.push('</div>')
+    }
+
+    htmlDescription.push('</div>')
+
+    return htmlDescription.join('')
   }
 }
